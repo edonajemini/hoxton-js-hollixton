e34aee9e093b72904c7d953319e44de2710990d1
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-07-15 01:06:28 +0200

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,355 +1,299 @@
 import './style.css'
 
 type StoreItem = {
-    id: number,
-    type: string,
-    name: string,
-    image: string,
-    price: number,
-    discountedPrice?: number,
-    dateEntered: string,
-    stock: number
+  id: number
+  title: string
+  price: number
+  image: string
+  discountedPrice?: number
 }
-
-type User = {
-    firstName: string,
-    lastName: string,
-    id: string,
-    password: string | number,
-    bag: []
-  }
 
 type State = {
-    storeItems: StoreItem[];
-    byType: string;
-    users: User[];
-    selectedItem: StoreItem | null
-    page: 'store' | 'item'
+  store: StoreItem[]
+  page: 'Hollixton' | 'guys' | 'girls' | 'sale'
+  modal: 'search' | 'bag' | 'profile'|''
+  filter: string
+  visitorCount: number
 }
 
-const state: State = {
-    storeItems: [],
-    byType: '',
-    users: [],
-    selectedItem: null,
-    page: 'store'
-}
-
-
-function selectedItem(item: StoreItem) {
-    state.selectedItem = item;
-  }
-  function deselectItem() {
-    state.selectedItem = null;
-  }
-
-function renderStoreItemsByTypeGirls(){
-    let mainEl = document.querySelector('main')
-    let girlsEl = document.querySelector('.girls-page')
-    girlsEl?.addEventListener('click', function (){
-    let storeItems = state.storeItems
-    let storeItemsHtml = ''
-    for(let item of storeItems){
-        if(item.type.toLowerCase() === 'girls' && item.discountedPrice){
-            storeItemsHtml += `
-            <div class="store-item">
-            <img src="${item.image}" alt="${item.name}" width="250">
-            <h3>${item.name}</h3>
-            <div class="discounted-price">
-            <p>$${item.price}</p>
-            <span>$${item.discountedPrice}</span>
-            </div>
-        </div>
-            `
-        }   
-        else if(item.type.toLowerCase() === 'girls' && item.discountedPrice === undefined){
-            storeItemsHtml += `
-            <div class="store-item">
-                <img src="${item.image}" alt="${item.name}" width="250">
-                <h3>${item.name}</h3>
-                <p>$${item.price}</p>
-            </div>
-            `
-            }
-        }
-        
-        mainEl.innerHTML = storeItemsHtml
-    })
-    
-}
-
-function renderStoreItemsByTypeGuys(){
-    let mainEl = document.querySelector('main')
-    let guysEl = document.querySelector('.guys-page')
-    guysEl?.addEventListener('click', function (){
-    let storeItems = state.storeItems
-    let storeItemsHtml = ''
-    for(let item of storeItems){
-        if(item.type.toLowerCase() === 'guys' && item.discountedPrice){
-            storeItemsHtml += `
-            <div class="store-item">
-            <img src="${item.image}" alt="${item.name}" width="250">
-            <h3>${item.name}</h3>
-            <div class="discounted-price">
-            <p>$${item.price}</p>
-            <span>$${item.discountedPrice}</span>
-            </div>
-        </div>
-            `
-        }   
-        else if(item.type.toLowerCase() === 'guys' && item.discountedPrice === undefined){
-            storeItemsHtml += `
-            <div class="store-item">
-                <img src="${item.image}" alt="${item.name}" width="250">
-                <h3>${item.name}</h3>
-                <p>$${item.price}</p>
-            </div>
-            `
-            }
-        }
-        
-        mainEl.innerHTML = storeItemsHtml
-    
-})
-}
-
-function renderStoreItemsByTypeSale(){
-    let mainEl = document.querySelector('main')
-    let saleEl = document.querySelector('.sale-page')
-    saleEl?.addEventListener('click', function (){
-    let storeItems = state.storeItems
-    let storeItemsHtml = ''
-    for(let item of storeItems){
-        if(item.discountedPrice){
-            storeItemsHtml += `
-            <div class="store-item">
-            <img src="${item.image}" alt="${item.name}" width="250">
-            <h3>${item.name}</h3>
-            <div class="discounted-price">
-            <p>$${item.price}</p>
-            <span>$${item.discountedPrice}</span>
-            </div>
-        </div>
-            `
-        }
+let state: State = {
+  store: [
+    { 
+      id: 1,
+      title: 'Shorts',
+      price: 3.2,
+      discountedPrice: 1,
+      image:
+        'https://cdn.shopify.com/s/files/1/1594/4353/products/Spring22-Mens-Session-Shorts-5inch-Denim.png?v=1646758994?q=50&auto=format&dpr=1&w=800&h=450&fit=crop'
+    },
+    {
+      id: 2,
+      title: 'Jeans',
+      price: 19.99,
+      image:
+        'https://media.gq-magazine.co.uk/photos/604a389378d908f40e6180ae/master/w_1920,h_1280,c_limit/Jeans_0006_Polo%20raplh%20lauren.jpg'
+    },
+    {
+      id: 3,
+      title: 'TV',
+      price: 2000.2,
+      discountedPrice: 8,
+      image:
+        'https://images.samsung.com/is/image/samsung/p6pim/uk/ue32t5300ckxxu/gallery/uk-fullhdtv-t5300-323348-ue32t5300ckxxu-421643304?$650_519_PNG$'
+    },
+    {
+      id: 4,
+      title: 'Radio',
+      price: 100.2,
+      discountedPrice: 40.5,
+      image:
+        'https://www.sony.co.uk/image/fc032c6f94c98631d6021a89dc17bfd7?fmt=pjpeg&bgcolor=FFFFFF&bgc=FFFFFF&wid=2515&hei=1320'
     }
-    mainEl.innerHTML = storeItemsHtml
-    })
+  ],
+  page: 'Hollixton',
+  modal: '',
+  filter: 'short',
+  visitorCount: 1123
 }
 
-function renderFilteredStoreItems(){
-    let mainEl = document.querySelector('main')
-    let searchEl = document.querySelector('.search-bar')
-    searchEl?.addEventListener('click', function(){
-        let searchInputEl = document.createElement('input')
-        searchInputEl.type = 'text';
-        searchInputEl.placeholder = 'Search the item name';
-        searchInputEl.className = 'search-input'
-        searchEl?.appendChild(searchInputEl)
-        searchInputEl.addEventListener('keyup', function(){
-            let searchInput = searchInputEl.value.toLowerCase()
-            let storeItems = state.storeItems
-            let storeItemsHtml = ''
-            for(let item of storeItems){
-                if(item.name.toLowerCase().includes(searchInput) && item.discountedPrice){
-                    storeItemsHtml += `
-                    <div class="store-item">
-                    <img src="${item.image}" alt="${item.name}" width="250">
-                    <h3>${item.name}</h3>
-                    <div class="discounted-price">
-                    <p>$${item.price}</p>
-                    <span>$${item.discountedPrice}</span>
-                    </div>
-                </div>
-                    `
-                } else if(item.name.toLowerCase().includes(searchInput) && item.discountedPrice === undefined){
-                    storeItemsHtml += `
-                    <div class="store-item">
-                        <img src="${item.image}" alt="${item.name}" width="250">
-                        <h3>${item.name}</h3>
-                        <p>$${item.price}</p>
-                    </div>
-                    `
-                    }
-                }
-            
-            mainEl.innerHTML = storeItemsHtml
-        })
-}, {once : true})
+function renderHeader (appEl: Element) {
+  let headerEl = document.createElement('header')
+  let headerH1 = document.createElement('h1')
+  headerH1.textContent = 'Hollixton'
+ 
 
-}
-function getStoreItems(){
-    fetch('http://localhost:3001/store')
-    .then(res => res.json())
-    .then(data => {
-        state.storeItems = data
-        render()
-    })
-}
-
-
-function renderStoreItems(){
-    let mainEl = document.querySelector('main')
-    let storeItems = state.storeItems
-    for(let item of storeItems){
-        if(item.discountedPrice){
-        let divEl = document.createElement('div')
-        divEl.className = 'store-item'
-        divEl.addEventListener('click', function(){
-            state.page = 'item'
-            selectedItem(item)
-            render()
-        })
-
-        let imgEl = document.createElement('img')
-        imgEl.src = item.image
-        imgEl.alt = item.name
-        imgEl.width = 250
-        let h3El = document.createElement('h3')
-        h3El.innerText = item.name
-        let div2El = document.createElement('div')
-        div2El.className = 'discounted-price'
-        let pEl = document.createElement('p')
-        pEl.innerText = `$${item.price}`
-        let spanEl = document.createElement('span')
-        spanEl.innerText = `$${item.discountedPrice}`
-        div2El.append(pEl, spanEl)
-        divEl.append(imgEl, h3El, div2El)
-        mainEl.append(divEl)
-        }
-        else {
-
-        let divEl = document.createElement('div')
-        divEl.className = 'store-item'
-        divEl.addEventListener('click', function(){
-            state.page = 'item'
-            selectedItem(item)
-            render()
-        })
-        let imgEl = document.createElement('img')
-        imgEl.src = item.image
-        imgEl.alt = item.name
-        imgEl.width = 250
-        let h3El = document.createElement('h3')
-        h3El.innerText = item.name
-        let pEl = document.createElement('p')
-        pEl.innerText = `$${item.price}`
-        divEl.append(imgEl, h3El, pEl)
-        mainEl.append(divEl)
-        }
-    }
-    state.page = 'store'
-}
-
-function renderSelectedItemPage(){
-    let mainEl = document.querySelector('main')
-    let selectedItem = state.selectedItem
-
-    let divEl = document.createElement('div')
-    divEl.className = 'selected-item'
-
-    let imgEl = document.createElement('img')
-    imgEl.src = selectedItem.image
-    imgEl.alt = selectedItem.name
-    imgEl.width = 500
-
-    
-
-    let infoEl = document.createElement('div')
-    infoEl.className = 'selected-item-info'
-
-    
-
-    let h3El = document.createElement('h3')
-    h3El.innerText = selectedItem.name
-    h3El.className = 'selected-item-name'
-
-    let priceEl = document.createElement('div')
-    priceEl.className = 'selected-item-price'
-
-    if(selectedItem?.dateEntered === "2021/07/10"){
-        let pEl = document.createElement('p')
-        pEl.innerText = `New!`
-        pEl.className = "recently-added"
-        priceEl.append(pEl)
-    }
-
-    if(selectedItem.discountedPrice){
-
-    let pEl = document.createElement('p')
-    pEl.innerText = `$${selectedItem.price}`
-    pEl.className = 'selected-item-price-alone'
-
-    let spanEl = document.createElement('span')
-    spanEl.innerText = `$${selectedItem.discountedPrice}`
-    spanEl.className = 'selected-discounted-price'
-    priceEl.append(pEl, spanEl)
-    } else {
-        let pEl = document.createElement('p')
-        pEl.innerText = `$${selectedItem.price}`
-        priceEl.append(pEl)
-    }
-
-    let buttonEl = document.createElement('button')
-    buttonEl.innerText = 'Add to Cart'
-    buttonEl.className = 'add-to-cart-button'
-
-    
-    infoEl.append(priceEl ,h3El, buttonEl)
-    divEl.append(imgEl, infoEl)
-    mainEl.append(divEl)
-        
-}
-
-function renderLogin(){
-    let mainEl = document.querySelector('main')
-    let loginEl = document.querySelector('.log-in')
-    loginEl?.addEventListener('click', function(){
-        let loginHtml = `
-        <div class="login-form">
-        <h2>Log In</h2>
-        <form>
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Email">
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Password">
-        <button type="submit">Log In</button>
-        </form>
-        </div>
-        `
-        mainEl.innerHTML = loginHtml
-    })
-}
-
-
-function render(){
-    let mainEl = document.querySelector('main')
-    mainEl.innerHTML = ''
-
-   
-
-    if(state.page === 'item'){
-        renderSelectedItemPage()
-    } else{
-
-    renderStoreItems()
-    renderStoreItemsByTypeGirls()
-    renderStoreItemsByTypeGuys()
-    renderStoreItemsByTypeSale()
-    renderLogin()
-    }
-}
-
-
-let logoEl = document.querySelector('.the-logo')
-logoEl?.addEventListener('click', function (){
-    deselectItem()
-    state.page = 'store'
+  let guysLink = document.createElement('a')
+  guysLink.textContent = 'Guys'
+  guysLink.addEventListener('click', function () {
+    state.page = 'guys'
     render()
-})
+  })
 
-renderFilteredStoreItems()
-getStoreItems()
+  let girlsLink = document.createElement('a')
+  girlsLink.textContent = 'Girls'
+  girlsLink.addEventListener('click', function () {
+    state.page = 'girls'
+    render()
+  })
+
+  let saleLink = document.createElement('a')
+  saleLink.textContent = 'Sale'
+  saleLink.addEventListener('click', function () {
+    state.page = 'sale'
+    render()
+  })
+
+  let searchModalButton = document.createElement('button')
+  searchModalButton.textContent = '🔍'
+  searchModalButton.addEventListener('click', function () {
+    state.modal = 'search'
+    render()
+  })
+
+  let profileModalButton = document.createElement('button')
+  profileModalButton.textContent = '👤'
+  profileModalButton.addEventListener('click', function () {
+    state.modal = 'profile'
+    render()
+  })
+
+  let bagModalButton = document.createElement('button')
+  bagModalButton.textContent = '👜'
+  bagModalButton.addEventListener('click', function () {
+    state.modal = 'bag'
+    render()
+  })
+
+  headerEl.append(
+    headerH1,
+    guysLink,
+    girlsLink,
+    saleLink,
+    searchModalButton,
+    profileModalButton,
+    bagModalButton,
+  )
+  appEl.append(headerEl)
+}
+
+function renderHomePage (appEl: Element) {
+  let HomePage = document.createElement('h3')
+  HomePage.textContent = 'Home'
+  let mainEl = document.createElement('main')
+
+  let storeListUl = document.createElement('ul')
+  storeListUl.className = 'store-list'
+
+  for (let item of state.store) {
+    let storeItemLi = document.createElement('li')
+    storeItemLi.className = 'store-item'
+
+    let titleEl = document.createElement('h3')
+    titleEl.textContent = item.title
+
+    let priceEl = document.createElement('p')
+    priceEl.textContent = `£${item.price.toFixed(2)}`
+
+    let imageEl = document.createElement('img')
+    imageEl.src = item.image
+
+    storeItemLi.append (titleEl, priceEl, imageEl)
+    storeListUl.append(storeItemLi)
+  }
+
+  mainEl.append(HomePage, storeListUl)
+  appEl.append(mainEl)
+}
+
+function renderGirlsPage (appEl: Element) {
+  let mainEl = document.createElement('main')
+
+  let titleEl = document.createElement('h1')
+  titleEl.textContent = 'Girls'
+
+  mainEl.append(titleEl)
+  appEl.append(mainEl)
+}
+
+function renderGuysPage (appEl: Element) {
+  let mainEl = document.createElement('main')
+
+  let titleEl = document.createElement('h1')
+  titleEl.textContent = 'Guys'
+
+  mainEl.append(titleEl)
+  appEl.append(mainEl)
+}
+
+function renderSalePage (appEl: Element) {
+  let mainEl = document.createElement('main')
+
+  let titleEl = document.createElement('h1')
+  titleEl.textContent = 'Sale'
+
+  mainEl.append(titleEl)
+  appEl.append(mainEl)
+}
+
+function renderSearchModal (appEl: Element) {
+  let wrapperEl = document.createElement('div')
+  wrapperEl.className = 'modal-wrapper'
+
+  let containerEl = document.createElement('div')
+  containerEl.className = 'modal-container'
+
+  let closeButton = document.createElement('button')
+  closeButton.textContent = 'X'
+  closeButton.className = 'modal-close-button'
+  closeButton.addEventListener('click', function () {
+    state.modal = ''
+    render()
+  })
+
+  let titleEl = document.createElement('h2')
+  titleEl.textContent = 'Search'
+
+  let formEl = document.createElement('form')
+  formEl.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    state.filter = inputEl.value
+    state.modal = ''
+    render()
+  })
+
+  let inputEl = document.createElement('input')
+  formEl.append(inputEl)
+
+  containerEl.append(closeButton, titleEl, formEl)
+  wrapperEl.append(containerEl)
+  appEl.append(wrapperEl)
+}
+
+function renderProfileModal (appEl: Element) {
+  let wrapperEl = document.createElement('div')
+  wrapperEl.className = 'modal-wrapper'
+
+  let containerEl = document.createElement('div')
+  containerEl.className = 'modal-container'
+
+  let closeButton = document.createElement('button')
+  closeButton.textContent = 'X'
+  closeButton.className = 'modal-close-button'
+  closeButton.addEventListener('click', function () {
+    state.modal = ''
+    render()
+  })
+
+  let titleEl = document.createElement('h2')
+  titleEl.textContent = 'Register'
+
+  let formEl = document.createElement('form')
+  formEl.addEventListener('register', function (event) {
+    event.preventDefault()
+
+    state.filter = inputEl.value
+    state.modal = ''
+    
+    render()
+  })
+  
+  let name = document.createElement('a')
+  name.textContent = 'Your Name:'
+  let inputEl = document.createElement('input')
+  formEl.append(inputEl)
+  
+
+
+  containerEl.append(closeButton, titleEl, name, formEl,)
+  wrapperEl.append(containerEl)
+  appEl.append(wrapperEl)
+}
+function renderBagModal (appEl: Element) {
+  let wrapperEl = document.createElement('div')
+  wrapperEl.className = 'modal-wrapper'
+
+  let containerEl = document.createElement('div')
+  containerEl.className = 'modal-container'
+
+  let closeButton = document.createElement('button')
+  closeButton.textContent = 'X'
+  closeButton.className = 'modal-close-button'
+  closeButton.addEventListener('click', function () {
+    state.modal = ''
+    render()
+  })
+
+  let ItemBag = document.createElement('h2')
+  ItemBag.textContent = 'Your Item Bag:'
+
+  let Items = document.createElement('img')
+  Items.textContent = '$storeListUl'
+  let ItemList = document.createElement('a')
+  ItemList.textContent = 'Your Item Bag:'
+  
+  containerEl.append(closeButton, ItemBag, Items,)
+  wrapperEl.append(containerEl)
+  appEl.append(wrapperEl)
+}
+function render () {
+  let appEl = document.querySelector<HTMLElement>('#app')
+  if (appEl === null) return
+  appEl.textContent = ''
+
+  renderHeader(appEl)
+
+  if (state.page === 'Hollixton') renderHomePage(appEl)
+  if (state.page === 'guys') renderGuysPage(appEl)
+  if (state.page === 'girls') renderGirlsPage(appEl)
+  if (state.page === 'sale') renderSalePage(appEl)
+
+  if (state.modal === 'search') renderSearchModal(appEl)
+  if (state.modal === 'profile') renderProfileModal(appEl)
+  if (state.modal === 'bag') renderBagModal(appEl)
+}
+
 render()
 
-
+window.state = state
+window.render = render
